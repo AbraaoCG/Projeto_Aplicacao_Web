571b08ad553e1b0f6292a7e5770ce3bd67af3246
Abraão Carvalho Gomes <abraao.cg@poli.ufrj.br>
2023-03-22 22:19:16 -0300

    Inclusão no projeto de um novo template automático para ser usado pelo flask, razoavelmente mais elegante.

--- MLwebApp.py
+++ MLwebApp.py
@@ -6,9 +6,12 @@ from werkzeug.utils import secure_filename
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+from plotly.graph_objs import *
+
 import plotly
 import json
 import subprocess
+import platform
 
 
 app = Flask(__name__)
@@ -24,6 +27,7 @@ os.makedirs(os.path.join(app.instance_path, 'htmlfi'), exist_ok=True)
 
 @app.route('/')
 def index(file_path = 'exampleData/exampleData2.csv'):
+    return verifyData(file_path)
     inputData = pd.read_csv(file_path)
     graphIN = getGraph(inputData, 'Dados Importados')
     return render_template('index.html', graphIN=graphIN)
@@ -44,14 +48,28 @@ def getGraph(data, title):
     xName = data.columns[0] ; yName = data.columns[-1]
     # data = data.sort_values(xName)
     fig = px.line(data.sort_values(xName), x=xName , y=yName, title = title)
+    layout = Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    font_color = 'rgba(255,255,255,1)',
+    # title_font = 'rgba(255,255,255,1)',
+    )
+    fig.layout = layout
     return json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
 def getGraph_Pred(inputData,data1,data2):
+    layout = Layout(
+    paper_bgcolor='rgba(0,0,0,0)',
+    plot_bgcolor='rgba(0,0,0,0)',
+    font_color = 'rgba(255,255,255,1)',
+    # title_font = 'rgba(255,255,255,1)',
+    )
+
     fig = make_subplots(rows=1,
                     cols=1,
-                    subplot_titles=('Predição com modelo de Regressão GD')) 
-
-
+                    subplot_titles=('Predição com modelo de Regressão GD'),
+                    ) 
+    fig.layout = layout
     xName = data1.columns[0] ; yName = data1.columns[-1] ; data = data1.sort_values(xName)
     # fig = px.line(data1.sort_values(xName), x=xName , y=yName, title = title, color = 'blue',name = 'Predição em treino')
     fig.add_trace( go.Scatter(x=data1[xName], y=data1[yName],
@@ -95,7 +113,10 @@ def runML_andPlot(inputData,file_path):
     with open ('FT_NN_data/dataPath_Len.txt', 'w') as text_file: # Export do caminho
         text_file.write(f'{file_path}\n{inputData.shape[0]}\n{inputData.shape[1]}' )
     #Chamada do Fortran
-    subprocess.call("./fortranML.out")
+    if ( platform.system() == 'Windows'):
+        subprocess.call("./NN_Process.exe")
+    else:
+        subprocess.call("./fortranML.out")
     # Preparação dos dados para Plots
     erroDF = pd.read_csv('FT_NN_data/output_erro.csv')
     yPredDF1 = pd.read_csv('FT_NN_data/output_Pred1.csv')
@@ -104,7 +125,7 @@ def runML_andPlot(inputData,file_path):
     graphIN = getGraph(inputData, 'Dados Importados')
     graphErro = getGraph(erroDF, 'Erro estimado ao longo das épocas')
     graphPred = getGraph_Pred(inputData,yPredDF1,yPredDF2)
-    return render_template('index.html', graphIN=graphIN, graphPred = graphPred, graphErro = graphErro)
+    return render_template('index2.html', graphIN=graphIN, graphPred = graphPred, graphErro = graphErro)
 
 
 
